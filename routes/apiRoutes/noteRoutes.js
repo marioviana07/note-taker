6cea73faa0875f39d91c6f15b6f95d97bd443da8
marioviana07 <mario_viana_2005@yahoo.com>
2020-08-25 12:12:29 -0700

    Add Heroku

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,7 @@
 const { filterByQuery, findById, createNewNote, deleteNote, validateNote } = require('../../lib/notes');
-const { notes } = require('../../db/db');
-const { resolveSoa } = require('dns');
+const { notes } = require('../../db/db.json');
+//const { resolveSoa } = require('dns');
+console.log("NOTES", notes)
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
@@ -21,6 +22,10 @@ router.get('/notes/:id', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
+    console.log("----")
+    console.log("HERE IS NOTES")
+    console.log(notes)
+    console.log("----")
     req.body.id = notes.length.toString();
     if (!validateNote(req.body)) {
         res.status(400).send('Incorrect Format');
@@ -30,17 +35,29 @@ router.post('/notes', (req, res) => {
     }
 });
 
+// router.post('/notes', (req, res) => {
+//     console.log(notes)
+//     req.body.id = notes.length.toString();
+
+//     if (!validateNote(req.body)) {
+//         res.status(400).send('Incorrect Format');
+//     } else {
+//         const note = createNewNote(req.body, notes);
+//         res.json(note);
+//     }
+// });
+
 router.delete('/notes/:id', function(req, res) {
-    const deleteNote = deleteNote({ id: req.params.id }, notes);
+    const deletedNote = deleteNote({ id: req.params.id }, notes);
     const result = {
         message: 'bad request',
         status_code: 400,
         body: null
     };
-    if (deleteNote) {
+    if (deletedNote) {
         result.message = "success";
         result.status_code = 200;
-        result.body = deleteNote;
+        result.body = deletedNote;
     }
 
     return res.json(result);
